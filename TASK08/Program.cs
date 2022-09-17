711d78667e05a6fd962cea8f0a5d0e28cae16897
Khetag <hetagm77@gmail.com>
2022-09-17 23:17:19 +0300

    add TASK08

---
+++ TASK08/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите натуральное число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while(current <= number)
+{
+    if(current % 2 == 0)
+    {
+        Console.Write(current + ";  ");
+    }
+    current = current + 1;
+    
+}
