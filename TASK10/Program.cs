c49f2218254ca888aac703c6516d89808c640d72
Khetag <hetagm77@gmail.com>
2022-09-19 20:59:18 +0300

    add HomeWork Tasks

---
+++ TASK10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int FindSecondNumber(int number)
+{
+    int left = number / 10;
+    //int right = number % 10;
+    int result = left % 10;
+    return result;
+}
+int randNumber = new Random().Next(100,1000);
+int metod = FindSecondNumber(randNumber);
+Console.WriteLine($"Случайное трехзначное число - {randNumber}");
+Console.WriteLine($"Результат задачи - {metod}");
---
+++ TASK13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void ThirdDigit(int number)
+{
+    string numberText = Convert.ToString(number);
+    if (numberText.Length > 2)
+        Console.WriteLine("Третья цифра - " + numberText[2]);
+    else
+        Console.WriteLine("Третьей цифры нет");
+}
+ThirdDigit(num);
